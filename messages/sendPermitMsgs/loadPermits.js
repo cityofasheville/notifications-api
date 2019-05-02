c827de08ba62c66edb6f98c04cf33cb19fc04fdd
Jon Wilson <jtwilson@ashevillenc.gov>
2019-05-02 16:01:57 -0400

    rewrite loadpermits as view

--- messages/sendPermitMsgs/loadPermits.js
+++ messages/sendPermitMsgs/loadPermits.js
@@ -16,42 +16,9 @@ async function loadPermits() {
     `);
     const tagRows = tags.rows;
     const permits = await mds_client.query(`
-    select * from (
-      select 'Minor' as tag, permit_num, applied_date, 
-      case when coalesce(applicant_name,'')='' Then address else applicant_name end as name, x, y
-      FROM simplicity.simplicity_permits_view
-      where permit_group = 'Planning'
-      and permit_type = 'Development'
-      and permit_subtype = 'Level I'
-      union
-      select 'Major' as tag, permit_num, applied_date, 
-      case when coalesce(applicant_name,'')='' Then address else applicant_name end as name, x, y
-      FROM simplicity.simplicity_permits_view
-      where permit_group = 'Planning'
-      and (
-        (permit_type = 'Subdivision' and permit_subtype = 'Major')
-        or
-        (permit_type = 'Development' and permit_subtype IN ('Level II','Level III','Conditional Zoning','Conditional Use'))
-        )
-      union
-      select distinct 'Affordable' as tag, simplicity_permits_view.permit_num, applied_date, 
-      case when coalesce(applicant_name,'')='' Then address else applicant_name end as name, x, y
-      FROM simplicity.simplicity_permits_view
-      inner join simplicity.permit_custom_fields
-      on simplicity_permits_view.permit_num = permit_custom_fields.permit_num
-      where permit_custom_fields.name ilike '%Affordable%'
-      and permit_custom_fields.value = 'Yes'
-      union
-      select distinct 'Slope' as tag, simplicity_permits_view.permit_num, applied_date, 
-      case when coalesce(applicant_name,'')='' Then address else applicant_name end as name, x, y
-      FROM simplicity.simplicity_permits_view
-      inner join simplicity.permit_custom_fields
-      on simplicity_permits_view.permit_num = permit_custom_fields.permit_num
-      where permit_custom_fields.name ilike '%Steep Slope%'
-      and permit_custom_fields.value = 'Yes' 
---      union select 'Slope', '06-05107', '2019-03-28'::date, 'testslope', -82.57691772, 35.57963755   
-  ) as inr
-  where applied_date >= NOW() - '16 days'::interval
+      SELECT tag, permit_num, applied_date, "name", x, y
+      FROM internal.notification_emails
+      where applied_date >= NOW() - '16 days'::interval
     `);
     for(row of permits.rows){
       const topics = await note_client.query(`
