9dfd70558064a3f229f611e09682fa8b95faa513
Jon Wilson <jtwilson@ashevillenc.gov>
2019-07-22 09:42:09 -0400

    deploy docs

--- deployment/redeploy.sh
+++ deployment/redeploy.sh
@@ -1,9 +1,7 @@
 #log in as apiadmin
 git pull
- #and then as root
-systemctl restart devnotifications1
-systemctl restart devnotifications2
-
-# or for production:
-systemctl restart notifications1
-systemctl restart notifications2
+ #and then as me
+sudo systemctl restart devnotifications1
+sudo systemctl restart devnotifications2
+sudo systemctl restart notifications1
+sudo systemctl restart notifications2
