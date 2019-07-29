2fb8a19d848a4f28db8ab04cb0e11f69de747fa6
Jon Wilson <jtwilson@ashevillenc.gov>
2019-07-29 11:25:51 -0400

    createuserPref accepts partial objects

--- api/api_resolvers.js
+++ api/api_resolvers.js
@@ -288,49 +288,89 @@ async function deleteTag(obj, args, context) {
 }
 
 async function createUserPreference(obj, args, context) {
+  let user_id;
+  let user_location_x;
+  let user_location_y;
   const client = await pool.connect();
   try {
-    //delete out any existing records for this email
-    for(send_type of args.user_preference.send_types){
-      const { rows } = await client.query(`  
-      delete from note.send_types where email = $1 returning user_id;
-      `, [send_type.email]);
-      if (rows[0]){
-        const ret = rows[0];
-        await client.query(`  
-        delete from note.subscriptions where user_id = $1;
-        `, [ret.user_id]);
-        await client.query(`  
-        delete from note.user_preferences where id = $1;
-        `, [ret.user_id]);
-      }
-    }
-    //insert
+    // see if they already exist
+    const emailsendtype = args.user_preference.send_types.find(function(sendtype) {
+      return sendtype.type = 'EMAIL';
+    }); 
     const { rows } = await client.query(`  
-    insert into note.user_preferences(location_x, location_y)VALUES($1, $2) returning id, location_x, location_y;
-    `, [ args.user_preference.location_x, args.user_preference.location_y ]);
-    const user_id = rows[0].id;
-    const user_location_x = rows[0].location_x;
-    const user_location_y = rows[0].location_y;
+    select send_types.user_id from note.send_types where email = $1;
+    `, [ emailsendtype.email ]);
+
+    if(rows[0]) { //already exist
+      user_id = rows[0].user_id;
+      if(args.user_preference.location_x && args.user_preference.location_y) {
+        await client.query(`
+        update note.user_preferences set location_x = $2, location_y = $3 where id = $1;
+        `, [ user_id, args.user_preference.location_x, args.user_preference.location_y]);
+        user_location_x = args.user_preference.location_x;
+        user_location_y = args.user_preference.location_y;  
+      }else{  //user exists but didn't send new x/y
+        const { rows } = await client.query(`
+        SELECT user_preferences.location_x, user_preferences.location_y
+        FROM note.user_preferences
+        WHERE user_preferences.id = $1
+        `, [user_id]);
+        user_location_x = rows[0].location_x;
+        user_location_y = rows[0].location_y;  
+      }
+    }else{ // new
+      const { rows } = await client.query(`  
+      insert into note.user_preferences(location_x, location_y)VALUES($1, $2) returning id, location_x, location_y;
+      `, [ args.user_preference.location_x, args.user_preference.location_y ]);
+      user_id = rows[0].id;
+      user_location_x = rows[0].location_x;
+      user_location_y = rows[0].location_y;      
+    }
+
     if(args.user_preference.send_types) {
-     for(send_type of args.user_preference.send_types){
-        const { rows } = await client.query(`  
+      for(send_type of args.user_preference.send_types){
+        await client.query(` 
         insert into note.send_types(
-            user_id, type, email, phone)
-          VALUES($1, $2, $3, $4)
-        `, [ user_id, send_type.type, send_type.email, 
-        send_type.phone ]);
+          user_id, type, email, phone)
+        VALUES($1, $2, $3, $4)
+        on conflict (user_id, type) do
+        update set email = $3, phone = $4
+        where send_types.user_id = $1 and send_types.type = $2;
+        `, [ user_id, send_type.type, send_type.email, send_type.phone ]);
       }
     }
+
     if(args.user_preference.subscriptions) {
+      // delete subscriptions not in array
+      const keepTags = args.user_preference.subscriptions.map(scrip=>parseInt(scrip.tag.id));
+      const res = await client.query(`
+      select * from note.subscriptions
+      where subscriptions.user_id = $1;
+      `, [ user_id ]);
+      for ( row of res.rows) {
+        if (keepTags.indexOf(row.tag_id) === -1) {
+          await client.query(`
+          delete from note.subscriptions
+          where subscriptions.user_id = $1
+          and subscriptions.tag_id = $2;
+          `, [ user_id, row.tag_id ])
+        }
+      };
       for(subscription of args.user_preference.subscriptions){
-        tag_id = subscription.tag.id;
-        const { rows } = await client.query(`  
+        await client.query(`
         insert into note.subscriptions(
-            user_id, tag_id, radius_miles, whole_city)
-          VALUES($1, $2, $3, $4)
-        `, [ user_id, tag_id, subscription.radius_miles, subscription.whole_city ]);
+          user_id, tag_id, radius_miles, whole_city)
+        VALUES($1, $2, $3, $4)
+        on conflict (user_id, tag_id) do
+        update set radius_miles = $3, whole_city = $4
+        where subscriptions.user_id = $1 and subscriptions.tag_id = $2
+        `, [ user_id, subscription.tag.id, subscription.radius_miles, subscription.whole_city ]);
       }
+    }else{
+      await client.query(`
+      delete from note.subscriptions
+      where subscriptions.user_id = $1;
+      `, [ user_id ]);
     }
     const ret = Object.assign({},args.user_preference,{id: user_id, location_x: user_location_x, location_y: user_location_y})
     return Promise.resolve(ret);
--- deployment/redeploy.sh
+++ deployment/redeploy.sh
@@ -5,3 +5,7 @@ sudo systemctl restart devnotifications1
 sudo systemctl restart devnotifications2
 sudo systemctl restart notifications1
 sudo systemctl restart notifications2
+
+
+systemctl restart devnotifications1
+systemctl restart devnotifications2
