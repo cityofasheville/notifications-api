0a056773f6f189e6371e4e80a2caf06023aec3e8
Nancie McCraw <nmccraw@ashevillenc.gov>
2021-01-29 09:41:26 -0500

    updated ci/cd config

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,14 +1,4 @@
-# Install the services
-cp ./notifications1.service /etc/systemd/system/
-cp ./notifications2.service /etc/systemd/system/
-
 # Start the services and enable starting after reboot
-systemctl start notifications1
-systemctl enable notifications1
-systemctl start notifications2
-systemctl enable notifications2
-
-# Install the NGINX configuration file
-cp ./notifications.conf /etc/nginx/conf.d/
-sudo systemctl restart nginx
+sudo systemctl restart notifications1
+sudo systemctl restart notifications2
 
