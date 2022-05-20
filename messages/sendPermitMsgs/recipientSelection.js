fb5c7b9f8a209947cc186e488fdd000b5d903ff8
Jon <jtwilson@ashevillenc.gov>
2022-05-20 11:50:34 -0400

    send email only to me, email testing

--- messages/sendPermitMsgs/recipientSelection.js
+++ messages/sendPermitMsgs/recipientSelection.js
@@ -17,27 +17,32 @@ async function recipientSelection() {
     const noteClient = await notePool.connect();
     try {
       const tags = await noteClient.query(`
-      select distinct send_types.type, send_types.email, send_types.phone, topics.name, topics.permit_num
-      from note.user_preferences
-      INNER JOIN note.send_types
-        ON user_preferences.id = send_types.user_id
-      INNER JOIN note.subscriptions
-        ON user_preferences.id = subscriptions.user_id
-      INNER JOIN note.tags
-        ON subscriptions.tag_id = tags.id
-      INNER JOIN note.topic_tags
-        ON tags.id = topic_tags.tag_id
-      INNER JOIN note.topics
-        ON topic_tags.topic_id = topics.id
-      where ( 
-          (subscriptions.whole_city = true) 
-          or 
-          (
-          (ST_Distance_Sphere(ST_MakePoint(user_preferences.location_x, user_preferences.location_y),ST_MakePoint(topics.location_x, topics.location_y)) / 1609.34) 
-          < subscriptions.radius_miles
-          )
-          )
-          ORDER BY type, email, name;
+      select type, email, phone, name, permit_num, STRING_AGG(notification_type,',') as notification_type from (
+        select distinct send_types.type, send_types.email, send_types.phone, topics.name, topics.permit_num, tags.name as notification_type
+        from note.user_preferences
+        INNER JOIN note.send_types
+          ON user_preferences.id = send_types.user_id
+        INNER JOIN note.subscriptions
+          ON user_preferences.id = subscriptions.user_id
+        INNER JOIN note.tags
+          ON subscriptions.tag_id = tags.id
+        INNER JOIN note.topic_tags
+          ON tags.id = topic_tags.tag_id
+        INNER JOIN note.topics
+          ON topic_tags.topic_id = topics.id
+        where 
+        send_types.email= 'jontwilson@gmail.com' and 
+        ( 
+            (subscriptions.whole_city = true) 
+            or 
+            (
+            (ST_DistanceSphere(ST_MakePoint(user_preferences.location_x, user_preferences.location_y),ST_MakePoint(topics.location_x, topics.location_y)) / 1609.34) 
+            < subscriptions.radius_miles
+            )
+        )
+       ) as subq
+       group by type, email, phone, name, permit_num
+      ORDER BY type, email, name;
       `);
       const tagRows = tags.rows;
 
