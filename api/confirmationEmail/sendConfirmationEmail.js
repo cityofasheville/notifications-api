9c5dadd7371847ccb0d0353fee27e622c6f7c0d4
Jon Wilson <jontwilson@gmail.com>
2023-04-11 12:37:11 -0400

    upg ses to 3

--- api/confirmationEmail/sendConfirmationEmail.js
+++ api/confirmationEmail/sendConfirmationEmail.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
-const { SES } = require("@aws-sdk/client-ses");
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses"); // CommonJS import
+const client = new SESClient({ region: 'us-east-1' });
+
 const pug = require('pug');
 const path = require('path');
 const Logger = require('coa-node-logging');
@@ -7,57 +9,52 @@ const cryptofuncs = require('../../api/cryptofuncs');
 
 require('dotenv').config({ path: path.join(__dirname, '/./../../.env') });
 
-AWS.config.update({ region: 'us-east-1' });
-
 const logFile = './logfile.log';
 const name = 'signup-confirmation-email';
 const logger = new Logger(name, logFile);
 
 const compiledFunction = pug.compileFile(path.join(__dirname, '/email.pug'));
 
-function sendConfirmationEmail(emailAddr) {
-  const recipient = {};
-  recipient.unsub_url = cryptofuncs.createUnsubUrl(emailAddr);
-  const htmlEmail = compiledFunction(recipient);
-  const params = {
-    Destination: { /* required */
-      ToAddresses: [emailAddr],
-    },
-    Message: { /* required */
-      Body: { /* required */
-        Html: {
-          Charset: 'UTF-8',
-          Data: htmlEmail,
+async function sendConfirmationEmail(emailAddr) {
+  try {
+    const recipient = {};
+    recipient.unsub_url = cryptofuncs.createUnsubUrl(emailAddr);
+    const htmlEmail = compiledFunction(recipient);
+    const params = {
+      Destination: { /* required */
+        ToAddresses: [emailAddr],
+      },
+      Message: { /* required */
+        Body: { /* required */
+          Html: {
+            Charset: 'UTF-8',
+            Data: htmlEmail,
+          },
+          Text: {
+            Charset: 'UTF-8',
+            Data: htmlEmail,
+          },
         },
-        Text: {
+        Subject: {
           Charset: 'UTF-8',
-          Data: htmlEmail,
+          Data: 'City of Asheville Notifications',
         },
       },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: 'City of Asheville Notifications',
-      },
-    },
-    Source: process.env.email_sender, /* required */
-    ReplyToAddresses: [
-      process.env.email_sender,
-    ],
-  };
+      Source: process.env.email_sender, /* required */
+      ReplyToAddresses: [
+        process.env.email_sender,
+      ],
+    };
 
-  // Create the promise and SES service object
-  const sendPromise = new SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
+    const command = new SendEmailCommand(params);
+    const response = await client.send(command);
 
-  // Handle promise's fulfilled/rejected states
-  sendPromise.then(
-    (data) => {
-      logger.info(`Email sent: ${emailAddr} ${data.MessageId}`);
-      console.log(`Email sent: ${emailAddr} ${data.MessageId}`);
-    },
-  ).catch((err) => {
+    logger.info(`Email sent: ${emailAddr} ${response.MessageId}`);
+    console.log(`Email sent: ${emailAddr} ${response.MessageId}`);
+  } catch (err) {
     logger.error(`Error sending email: ${emailAddr} Err: ${err}`);
     console.log(`Error sending email: ${emailAddr} Err: ${err}`);
-  });
+  }
 }
-
 module.exports = sendConfirmationEmail;
+
--- messages/sendPermitMsgs/sesSendemail.js
+++ messages/sendPermitMsgs/sesSendemail.js
@@ -1,47 +1,46 @@
-const { SES } = require("@aws-sdk/client-ses");
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses"); // CommonJS import
+const client = new SESClient({ region: 'us-east-1' });
+
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/./../../.env') });
 
-AWS.config.update({ region: 'us-east-1' });
-
-function sesSendemail(emailAddr, htmlEmail, callback) {
-  const params = {
-    Destination: { /* required */
-      ToAddresses: [emailAddr],
-    },
-    Message: { /* required */
-      Body: { /* required */
-        Html: {
-          Charset: 'UTF-8',
-          Data: htmlEmail,
+async function sesSendemail(emailAddr, htmlEmail) {
+  try {
+    const params = {
+      Destination: { /* required */
+        ToAddresses: [emailAddr],
+      },
+      Message: { /* required */
+        Body: { /* required */
+          Html: {
+            Charset: 'UTF-8',
+            Data: htmlEmail,
+          },
+          Text: {
+            Charset: 'UTF-8',
+            Data: htmlEmail,
+          },
         },
-        Text: {
+        Subject: {
           Charset: 'UTF-8',
-          Data: htmlEmail,
+          Data: 'City of Asheville Notifications',
         },
       },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: 'City of Asheville Notifications',
-      },
-    },
-    Source: process.env.email_sender, /* required */
-    ReplyToAddresses: [
-      process.env.email_sender,
-    ],
-  };
+      Source: process.env.email_sender, /* required */
+      ReplyToAddresses: [
+        process.env.email_sender,
+      ],
+    };
 
-  // Create the promise and SES service object
-  const sendPromise = new SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
+    const command = new SendEmailCommand(params);
+    const response = await client.send(command);
 
-  // Handle promise's fulfilled/rejected states
-  sendPromise.then(
-    (data) => {
-      callback(`Email sent: ${emailAddr} ${new Date().toISOString()} ${data.MessageId}`);
-    },
-  ).catch((err) => {
-    callback(`Error sending email: ${emailAddr} Err: ${err}`);
-  });
+    logger.info(`Email sent: ${emailAddr} ${response.MessageId}`);
+    console.log(`Email sent: ${emailAddr} ${new Date().toISOString()} ${response.MessageId}`);
+  } catch (err) {
+    logger.error(`Error sending email: ${emailAddr} Err: ${err}`);
+    console.log(`Error sending email: ${emailAddr} Err: ${err}`);
+  }
 }
 
 module.exports = sesSendemail;
